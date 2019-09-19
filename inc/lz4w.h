16eec95a6b9c92bfcb96992ce8f16073a5367c4d
nonarkitten <nonarkitten@gmail.com>
2019-09-19 14:57:22 -0600

    More cleanup with lz4w

--- inc/lz4w.h
+++ inc/lz4w.h
@@ -1,7 +1,8 @@
 #ifndef __LZ4W_H__
 #define __LZ4W_H__
 
-inline unsigned int lz4w_length(void *source) { return *(unsigned int*)source; }
+extern unsigned int lz4w_length(void *source);
 extern unsigned int lz4w_unpack(void *source, void *destination);
+extern void* lz4w_pack(void* source, int size, int* out_size);
 
 #endif
--- lz4w/lz4w.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LZ4W_H__
-#define __LZ4W_H__
-
-extern unsigned int lz4w_length(void *source);
-extern unsigned int lz4w_unpack(void *source, void *destination);
-extern void* lz4w_pack(void* source, int size, int* out_size);
-
-#endif
