a52aac78d77b8426100116d23b4454a029405ec2
nonarkitten <nonarkitten@gmail.com>
2019-09-19 16:52:17 -0600

    Moved config

---
+++ inc/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+//typedef unsigned long uint32_t_t_t;
+//typedef uint16_t uint16_t_t;
+//typedef uint8_t uint8_t_t;
+//typedef uint8_t byte;
+
+#define HAVE_ATEXIT 1
+
+#define ALIGNLONGS 1
+
+#define DIRECTRAM 1
+
+/* Version number of package */
+#define VERSION "0.7a"
+#define DATA_DIRECTORY "."
+
+#endif
